6eb1e9dfaf48c2cfbe0c081f6e6fdde9497425e6
Daniil Muntyan <30291019+DaniilMuntyan@users.noreply.github.com>
2020-05-18 15:55:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from threading import Thread
 
 
 def sign_in(login, password, base_url):
-    driver = webdriver.Opera(executable_path=r'D:\Python\Парсинг\Eolymp\operadriver.exe')
+    driver = webdriver.Opera(executable_path=r'path\operadriver.exe')
     driver.get(base_url)
     WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id('username'))
     driver.find_element_by_id('username').send_keys(login)
@@ -25,24 +25,13 @@ def main_function():
     base_url = r'https://www.e-olymp.com/ru/login'
     url = r'https://www.e-olymp.com/ru/users/muntyan/punchcard'
 
-    # options = webdriver.ChromeOptions()
-    # options.add_argument("user-data-dir=" + "C:\Users\Админ\AppData\Local\Google\Chrome\User Data")
-    # driver = webdriver.Chrome(executable_path='D:\chromedriver_win32\chromedriver.exe', options=options)
-    # driver.get(url)
-
     opera_profile = r'C:\Users\Админ\AppData\Roaming\Opera Software\Opera Stable'
     options = webdriver.ChromeOptions()
     options.add_argument('user-data-dir=' + opera_profile)
     options._binary_location = r'C:\Program Files (x86)\Opera\66.0.3515.72\opera.exe'
-    driver = webdriver.Opera(executable_path=r'D:\Python\Парсинг\Eolymp\operadriver.exe', options=options)
+    driver = webdriver.Opera(executable_path=r'path\operadriver.exe', options=options)
 
     driver.get(url)
-
-    '''wait = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id('username'))
-    driver.find_element_by_id('username').send_keys('login')
-    driver.find_element_by_id('password').send_keys('password')
-    driver.find_element_by_xpath("//input[@type='submit']").click()
-    driver.get(url)'''
 
     WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id('punch-card'))
 
@@ -69,7 +58,7 @@ def main_function():
     part = int(len(list) / n)
 
     for i in range(0, (n - 1)):
-        drivers.append(sign_in('muntyan', 'Wusu0813', base_url))
+        drivers.append(sign_in("login", 'pswd', base_url))
 
     k = 0
     i = 0
